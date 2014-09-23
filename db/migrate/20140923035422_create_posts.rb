9f35b97bf15da1696ae4bd596e5e0949a6a34439
abalone0204 <abalone0204@gmail.com>
2014-09-23 13:26:46 +0800

    新增posts這個資料表

---
+++ db/migrate/20140923035422_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :note
+      t.string :source
+
+      t.timestamps
+    end
+  end
+end

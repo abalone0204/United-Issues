6ced764d04f08fc647e8b99b3acb19450167e583
abalone0204 <abalone0204@gmail.com>
2014-09-25 21:14:05 +0800

    完成post的crud

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    
+  end
+
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else 
+      flash[:error] = "您輸入的資料有誤"
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+    if @post.update(post_params)
+      redirect_to @post
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    
+    @post.destroy
+    redirect_to posts_path 
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :note, :source)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+end

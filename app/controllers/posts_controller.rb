4257c62fac39f51d4347e194acf08b6229171f49
abalone0204 <abalone0204@gmail.com>
2014-09-28 21:25:25 +0800

    用devise實作使用者驗證機制

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :new, :destroy]
   
   def index
     @posts = Post.all

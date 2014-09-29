5070c82d77fb14fd28087bad7203081e33849d12
abalone0204 <abalone0204@gmail.com>
2014-09-29 16:37:29 +0800

    加入devise的tests helper進去spec helper裡面

--- spec/fabricators/post_fabricator.rb
+++ spec/fabricators/post_fabricator.rb
@@ -1,9 +1,11 @@
 Fabricator(:post) do
+  user { Fabricate(:user) }
   title { Faker::Lorem.characters(20) }
   content { Faker::Lorem.paragraph(3) }
 end
 
 
 Fabricator(:invalid_post, :from => :post) do
+  user { Fabricate(:user) }
   title { Faker::Lorem.characters(51) }
 end

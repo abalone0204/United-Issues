e3a8f8294c78028dca4657ac1b4b56133cf2cc53
abalone0204 <abalone0204@gmail.com>
2014-10-04 15:44:10 +0800

    move the render title method from model to helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,14 @@
 module PostsHelper
+  
   def render_post_tags(post)
     post.tags.map(&:name).map { |t| link_to t, tag_path(t) }.join(', ')
+  end
+
+  def render_post_title(post)
+    if post.country_classification.present?
+      "【#{post.country_classification.text}】 #{post.title}"
+    else
+      "【尚未分類】 #{post.title}"
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,26 +1,22 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, length: {maximum: 50}
+  
   extend Enumerize
   extend ActiveModel::Naming
+
+  validates :title, presence: true, length: {maximum: 50}
+
+  # Association 
+
   has_many :taggings
   has_many :tags, through: :taggings
 
-  enumerize :country_classification,
-    in: %w[Franch Russia Arab Germany Korean Spanish Japan Polish Czech Turkey]
   belongs_to :user
 
-  def render_title
-    if country_classification.present?
-      "【#{country_classification.text}】 #{title}"
-    else
-      "【尚未分類】 #{title}"
-    end
-
-  end
+  enumerize :country_classification,
+    in: %w[Franch Russia Arab Germany Korean Spanish Japan Polish Czech Turkey]
 
 
   # Tagging
-
   def self.tagged_with(name)
     Tag.find_by_name!(name).posts
   end

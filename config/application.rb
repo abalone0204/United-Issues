6c45a828b345708539c50758880b316c3ad606a1
abalone0204 <abalone0204@gmail.com>
2014-10-05 23:18:00 +0800

    Add page/*.js to precompile and set page/posts.js

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module UnitedIssues
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :'zh-TW'
+
+    config.assets.precompile += %w[page/*.js]
   end
 end

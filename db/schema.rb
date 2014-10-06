5907ce66c5803cde3f338b77a9dfbdd817b5e770
abalone0204 <abalone0204@gmail.com>
2014-10-06 19:07:51 +0800

    Add image column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141005130005) do
+ActiveRecord::Schema.define(version: 20141006091350) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141005130005) do
     t.boolean  "publish",                default: false
     t.text     "note",                   default: ""
     t.date     "publish_date"
+    t.string   "image"
   end
 
   create_table "taggings", force: true do |t|

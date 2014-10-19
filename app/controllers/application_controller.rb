0cc0b475f5a508e20c86ff960f97366c4814e797
abalone0204 <abalone0204@gmail.com>
2014-10-19 22:30:53 +0800

    Add the dropdown navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :get_country_classification
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
@@ -10,6 +12,10 @@ class ApplicationController < ActionController::Base
 
 
   protected
+
+  def get_country_classification
+    @country_classifications = Post.country_classification.options
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name

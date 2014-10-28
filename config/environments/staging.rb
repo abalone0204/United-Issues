9b3911c1ac9016666a955ac63b69f273493ab96b
abalone0204 <abalone0204@gmail.com>
2014-10-28 19:13:11 +0800

    precompile

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -30,8 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile += %w(images/*.png images/*.jpg images/*.jpeg images/*.gif)
-
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf)
   # Generate digests for assets URLs.
   config.assets.digest = true
 

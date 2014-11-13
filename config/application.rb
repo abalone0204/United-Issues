77a789ca4b88fcfa167809e5f74f69922eecf17e
abalone0204 <abalone0204@gmail.com>
2014-11-13 12:37:56 +0800

    fix assets

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module UnitedIssues
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = "Taipei"
-
+    config.serve_static_assets = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :'zh-TW'

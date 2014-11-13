2b627813c41025fa45030b9212e60b52dcaee673
abalone0204 <abalone0204@gmail.com>
2014-11-13 14:25:59 +0800

    add reset info to add posts count migrate

--- db/migrate/20141113033044_add_posts_count_to_users.rb
+++ db/migrate/20141113033044_add_posts_count_to_users.rb
@@ -1,6 +1,7 @@
 class AddPostsCountToUsers < ActiveRecord::Migration
   def change
     add_column :users, :posts_count, :integer, default: 0
+    User.reset_column_information
     User.all.each do |user|
       user.update_attribute(:posts_count, user.posts.length)
     end

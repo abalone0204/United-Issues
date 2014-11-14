92d520c42385ccadbffd4e589430d47ffda88214
abalone0204 <abalone0204@gmail.com>
2014-11-14 16:53:49 +0800

    pending

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
+    binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]

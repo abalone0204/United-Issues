ca7687e0c3bdec8398912f21d37cddab4908f4c0
abalone0204 <abalone0204@gmail.com>
2014-11-26 21:28:03 +0800

    done import

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,10 +22,16 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    if params[:found_post].present?
+      import_hash = params[:found_post]
+      import_hash_value(@post, import_hash)
+    end
+
 
   end
 
   def create
+
     @post = Post.new(post_params)
     if @post.save
       redirect_to @post
@@ -66,7 +72,14 @@ class PostsController < ApplicationController
 
   private
 
-
+  def import_hash_value(post, import_hash)
+    post.title = import_hash[:title]
+    post.original_author =import_hash[:original_author]
+    post.country_classification = import_hash[:country_classification]
+    post.remote_image_url = import_hash[:remote_image_url]
+    post.source_date = import_hash[:source_date]
+    post.source =import_hash[:source]
+  end
 
   def validate_publish
     if !@post.publish? && @post.user_id != current_user.id
--- app/helpers/finders_helper.rb
+++ app/helpers/finders_helper.rb
@@ -1,2 +1,12 @@
 module FindersHelper
+  def import_found_post_to_post(found_post)
+    hash = {}
+    hash[:title] =found_post.title
+    hash[:original_author] =found_post.original_author
+    hash[:country_classification] =found_post.finder.country_classification
+    hash[:remote_image_url] = found_post.image
+    hash[:source_date] = found_post.released_date.to_s
+    hash[:source] =found_post.source
+    return hash
+  end
 end

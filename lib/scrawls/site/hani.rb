e05cb0b548feb8412b8d5fce228e6c5425051a6f
abalone0204 <abalone0204@gmail.com>
2014-11-28 15:50:30 +0800

    done hani topnews

--- lib/scrawls/site/hani.rb
+++ lib/scrawls/site/hani.rb
@@ -1,9 +1,20 @@
 module Hani
   class HaniNews
-    
+    def self.scrap_top_news(top_news_data, url)
+      top_news = {}
+        top_news[:title] = top_news_data.at_css(".main-title img")['alt']
+        top_news[:description] = top_news_data.at_css(".preface-title").text.strip.gsub(/\r/, "").gsub(/\n/, " ")
+        top_news[:image] = top_news_data.at_css(".photo img")['src']
+        top_news[:source] = url+top_news_data.at_css("a")['href']
+        top_news[:released_date] =Date.today
+        result.push(top_news)
+
+      
+    end
   end
 
   def self.hani_scraping(data, url)
-    # top_news_data   
+    top_news_data = data.at_css(".type1")
+    HaniNews::scrap_top_news(top_news_data, url)
   end
 end

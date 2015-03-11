8477f6024b38840a20994cfa4b3022d74b5ed64f
abalone0204 <abalone0204@gmail.com>
2015-03-11 18:14:23 +0800

    change server ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,8 +7,8 @@ set :branch, "staging"
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
 # # specify server DNS name, user & roles
-server '54.64.218.2', user: 'ubuntu', roles: %w{web app db},ssh_options: {
-  port: 22, keys:["/Users/kudenny/Amazon/denny.pem"]
+server '54.92.122.231', user: 'ubuntu', roles: %w{web app db},ssh_options: {
+  port: 22, keys:["/Users/kudenny/Amazon/ui_production.pem"]
 }, primary: true
 # server '192.168.0.9', user: 'vagrant', roles: %w{web app db}, primary: true
 
